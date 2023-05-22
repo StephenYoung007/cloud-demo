e1f5bd25e56b3b0d9c023c49c46c9758b05819df
wy1309374767@gmail.com <wang1314@@%>
2023-05-23 00:16:43 +0800

    add sql script

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 调用restTemplate发起http请求
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://user-service/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
         // 4.返回
